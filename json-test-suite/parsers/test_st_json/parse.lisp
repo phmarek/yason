8ebd74df8d125c4f08bce5f1bb1b0958399ade3a
Ralph Schleicher <rs@ralph-schleicher.de>
2022-12-26 15:11:17 +0100

    ST-JSON reads from a stream.

--- json-test-suite/parsers/test_st_json/parse.lisp
+++ json-test-suite/parsers/test_st_json/parse.lisp
@@ -1,6 +1,7 @@
 (in-package #:de.ralph-schleicher.json-test-suite)
 
 (defun parse (file-name)
-  (st-json:read-json file-name))
+  (with-open-file (stream file-name)
+    (st-json:read-json stream)))
 
 ;;; parse.lisp ends here
