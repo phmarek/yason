0a36dac45b6d0c65fc8325054eb2eb98b75e74b2
Alexander Artemenko <svetlyak.40wt@gmail.com>
2024-10-08 21:39:24 +0000

    Updated render-doc with instruction what should be installed to make it work.

--- render-doc.sh
+++ render-doc.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# To make this script work on Ubuntu, run this command to install required packages:
+# apt install xsltproc libxml2-utils
+
 xmllint --noout doc.xml
 [ -f clixdoc.xsl ] || wget -q https://raw.github.com/hanshuebner/clixdoc/master/clixdoc.xsl
 xsltproc --stringparam current-release `perl -ne 'if (/^ *:version +"(.*)"/) { print "$1\n" }' yason.asd` -o index.html clixdoc.xsl doc.xml 
